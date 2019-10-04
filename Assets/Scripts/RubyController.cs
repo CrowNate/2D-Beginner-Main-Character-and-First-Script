674c289d212c1311428bc89ef5bf495356ddf9d2
Nathaniel <nate@fishwheel.org>
2019-10-03 22:22:11 -0500

    Added background art
    Built a world

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,15 +7,30 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
+        // This line of code is used for the "Left to Right" inputs in-game
+        float horizontal = Input.GetAxis("Horizontal");
 
+        // This line of code is used for the "Up to Down" inputs in-game
+        float vertical = Input.GetAxis("Vertical");
+
+
+        Vector2 position = transform.position;
+
+
+        // This line of code is determining the speed the sprite moves from left to right
+        // The number in front of "f" will determine the speed
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+
+        // This line of code is determining the speed the sprite moves from up to down
+        // The number in front of "f" will determine the speed
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        transform.position = position;
     }
 }
