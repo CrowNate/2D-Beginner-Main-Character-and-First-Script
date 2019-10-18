04a2e9d725a6127b70c3b537f2bbad77aebded00
Nathaniel <nate@fishwheel.org>
2019-10-17 23:36:29 -0500

    Added animation and projectiles
    Added animation and projectiles

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -8,6 +8,8 @@ public class RubyController : MonoBehaviour
 
     public int maxHealth = 5;
 
+    public GameObject projectilePrefab;
+
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -16,10 +18,15 @@ public class RubyController : MonoBehaviour
     float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
         currentHealth = maxHealth;
     }
@@ -34,16 +41,24 @@ public class RubyController : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         Vector2 position = rigidbody2d.position;
 
 
         // This line of code is determining the speed the sprite moves from left to right
         // The number in front of "f" will determine the speed
-        position.x = position.x + movementspeed * horizontal * Time.deltaTime;
-
-        // This line of code is determining the speed the sprite moves from up to down
-        // The number in front of "f" will determine the speed
-        position.y = position.y + movementspeed * vertical * Time.deltaTime;
+        position = position + move * movementspeed * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
 
@@ -53,6 +68,11 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+
     }
 
 
@@ -70,5 +90,17 @@ public class RubyController : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
 
         Debug.Log(currentHealth + "/" + maxHealth);
+
+        
+    }
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+
     }
 }
