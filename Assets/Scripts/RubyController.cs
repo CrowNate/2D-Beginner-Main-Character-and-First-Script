548e3590d1b0954d9f43c0ca3525d7925e3d1a25
Nathaniel <nate@fishwheel.org>
2019-10-31 18:19:17 -0500

    Build and Audio
    Changes to text, audio and build

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,13 +7,14 @@ public class RubyController : MonoBehaviour
     public float movementspeed = 3.0f;
 
     public int maxHealth = 5;
-
+    public float timeInvincible = 2.0f;
     public GameObject projectilePrefab;
+
+    public AudioClip throwSound;
+    public AudioClip hitSound;
 
     public int health { get { return currentHealth; } }
     int currentHealth;
-
-    public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
 
@@ -22,6 +23,8 @@ public class RubyController : MonoBehaviour
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
 
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +32,12 @@ public class RubyController : MonoBehaviour
         animator = GetComponent<Animator>();
 
         currentHealth = maxHealth;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 
     // Update is called once per frame
@@ -72,6 +81,18 @@ public class RubyController : MonoBehaviour
         {
             Launch();
         }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
 
     }
 
@@ -85,6 +106,7 @@ public class RubyController : MonoBehaviour
 
             isInvincible = true;
             invincibleTimer = timeInvincible;
+            PlaySound(hitSound);
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
@@ -101,6 +123,8 @@ public class RubyController : MonoBehaviour
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
+        PlaySound(throwSound);
 
     }
+
 }
